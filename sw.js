f7aff60867e81934621b9e8efd68b058d88ccc22
antisocialyomen <30310559+antisocialyomen@users.noreply.github.com>
2026-08-16 17:17:42 +0800

    进入页问候名可编辑：contenteditable 名字，失焦保存到 localStorage

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'workbench-v6';
+const CACHE = 'workbench-v7';
 const PRECACHE = ['/', '/index.html', '/manifest.json', '/assets/bg-wallpaper.jpg', '/assets/labubu-avatar.jpg'];
 
 self.addEventListener('install', e => {
