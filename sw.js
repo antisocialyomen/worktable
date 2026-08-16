e479c5518446da7e93dd294e0d6c9440b1a9944a
antisocialyomen <30310559+antisocialyomen@users.noreply.github.com>
2026-08-16 17:54:33 +0800

    数据页专注时长(h)改回一行显示；sw v12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'workbench-v11';
+const CACHE = 'workbench-v12';
 const PRECACHE = ['/', '/index.html', '/manifest.json', '/assets/bg-wallpaper.jpg', '/assets/labubu-avatar.jpg'];
 
 self.addEventListener('install', e => {
